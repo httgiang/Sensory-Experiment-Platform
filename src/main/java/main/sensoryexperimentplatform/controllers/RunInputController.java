3f4e9943d395912c50472a6b06a10a72e4a973b1
Quỳnh Anh <quynhanhhoang572004@gmail.com>
2024-08-13 18:51:16 +0700

    UI| Adding tool tip for Run Input and run controller

--- src/main/java/main/sensoryexperimentplatform/controllers/RunInputController.java
+++ src/main/java/main/sensoryexperimentplatform/controllers/RunInputController.java
@@ -2,9 +2,15 @@ package main.sensoryexperimentplatform.controllers;
 
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 import main.sensoryexperimentplatform.viewmodel.RunInputVM;
 
 public class RunInputController {
@@ -27,6 +33,48 @@ public class RunInputController {
         if(viewModel.getPlayAlert().get()){
             viewModel.playAlert();
         }
+        Tooltip tooltip = new Tooltip("Help text here!");
+        if (viewModel.getHelpText().get() != null) {
+            tooltip.setText(viewModel.getHelpText().get());
+        }
+
+        tooltip.setStyle(
+                "-fx-background-color: #e3e2e2;\n" +
+                        "    -fx-text-fill: #397E82;\n" +
+                        "    -fx-font-size: 20px;\n" +
+                        "    -fx-padding: 5px;\n" +
+                        "    -fx-border-color: White;\n" +
+                        "    -fx-border-width: 1px;\n" +
+                        "    -fx-border-radius: 3px;"
+        );
+        tooltip.setShowDelay(Duration.ZERO);
+        tooltip.setAutoHide(true);
+        tooltip.setWrapText(true);
+        tooltip.setMaxWidth(250);
+
+        // Set listeners to show and hide tooltip on mouse enter and exit
+        help_image.setOnMouseEntered(event -> showTooltip(help_image, tooltip));
+        help_image.setOnMouseExited(event -> tooltip.hide());
+
+        // Add a listener to detect when the image is set to the ImageView
+        help_image.imageProperty().addListener(new ChangeListener<>() {
+            @Override
+            public void changed(ObservableValue<? extends Image> observable, javafx.scene.image.Image oldImage, javafx.scene.image.Image newImage) {
+                if (newImage != null) {
+                    showTooltip(help_image, tooltip);
+                }
+            }
+        });
+
+
+
+    }
+    private void showTooltip(ImageView imageView, Tooltip tooltip) {
+        // Get the bounds of the ImageView
+        javafx.geometry.Bounds bounds = imageView.localToScreen(imageView.getBoundsInLocal());
+
+        // Show the tooltip at the top-left position of the ImageView
+        tooltip.show(imageView, bounds.getMinX() - 250, bounds.getMinY() - tooltip.getHeight());
     }
 
     private void bindViewModel(){
