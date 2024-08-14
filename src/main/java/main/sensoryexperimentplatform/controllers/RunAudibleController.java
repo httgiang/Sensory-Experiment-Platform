0c96e42401fbbf6bcdc0622fe58282ac75aeeb6b
Quỳnh Anh <quynhanhhoang572004@gmail.com>
2024-08-14 16:42:43 +0700

    UI| Adding help text button for Notice stage, Audible stage

--- src/main/java/main/sensoryexperimentplatform/controllers/RunAudibleController.java
+++ src/main/java/main/sensoryexperimentplatform/controllers/RunAudibleController.java
@@ -1,15 +1,23 @@
 package main.sensoryexperimentplatform.controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 import main.sensoryexperimentplatform.viewmodel.RunAudible_VM;
-import main.sensoryexperimentplatform.viewmodel.RunNotice_VM;
+
 
 public class RunAudibleController {
 
 
     @FXML
     private Label contentTxt;
+    @FXML
+    private ImageView help_image;
 
     @FXML
     private Label titleTxt;
@@ -18,6 +26,45 @@ public class RunAudibleController {
     public void setViewModel(RunAudible_VM viewModel){
         this.viewModel = viewModel;
         bindViewModel();
+        Tooltip tooltip = new Tooltip("Help text here!");
+        if (viewModel.getHelpText().get() != null) {
+            tooltip.setText(viewModel.getHelpText().get());
+        }
+
+        tooltip.setStyle(
+                "-fx-background-color: #e3e2e2;\n" +
+                        "    -fx-text-fill: #397E82;\n" +
+                        "    -fx-font-size: 20px;\n" +
+                        "    -fx-padding: 5px;\n" +
+                        "    -fx-border-color: White;\n" +
+                        "    -fx-border-width: 1px;\n" +
+                        "    -fx-border-radius: 3px;"
+        );
+        tooltip.setShowDelay(Duration.ZERO);
+        tooltip.setAutoHide(true);
+        tooltip.setWrapText(true);
+        tooltip.setMaxWidth(250);
+
+        // Set listeners to show and hide tooltip on mouse enter and exit
+        help_image.setOnMouseEntered(event -> showTooltip(help_image, tooltip));
+        help_image.setOnMouseExited(event -> tooltip.hide());
+
+        // Add a listener to detect when the image is set to the ImageView
+        help_image.imageProperty().addListener(new ChangeListener<>() {
+            @Override
+            public void changed(ObservableValue<? extends Image> observable, javafx.scene.image.Image oldImage, javafx.scene.image.Image newImage) {
+                if (newImage != null) {
+                    showTooltip(help_image, tooltip);
+                }
+            }
+        });
+    }
+    private void showTooltip(ImageView imageView, Tooltip tooltip) {
+        // Get the bounds of the ImageView
+        javafx.geometry.Bounds bounds = imageView.localToScreen(imageView.getBoundsInLocal());
+
+        // Show the tooltip at the top-left position of the ImageView
+        tooltip.show(imageView, bounds.getMinX() - 250, bounds.getMinY() - tooltip.getHeight());
     }
     private void bindViewModel(){
         titleTxt.textProperty().bind(viewModel.getTitle());
--- src/main/java/main/sensoryexperimentplatform/viewmodel/RunNotice_VM.java
+++ src/main/java/main/sensoryexperimentplatform/viewmodel/RunNotice_VM.java
@@ -9,6 +9,7 @@ import main.sensoryexperimentplatform.models.Notice;
 public class RunNotice_VM {
     private Notice notice;
     private StringProperty title, content, button;
+    private StringProperty helpText;
     private BooleanProperty alert;
 
     public RunNotice_VM(Notice notice){
@@ -16,6 +17,7 @@ public class RunNotice_VM {
         button = new SimpleStringProperty(notice.getButtonText());
         title = new SimpleStringProperty(notice.getTitle());
         content = new SimpleStringProperty(notice.getContent());
+        helpText = new SimpleStringProperty(notice.getHelpText());
         alert = new SimpleBooleanProperty(notice.isAlert());
     }
     public StringProperty getTitle(){
@@ -26,6 +28,9 @@ public class RunNotice_VM {
     }
     public StringProperty buttonProperty() {
         return button;
+    }
+    public StringProperty getHelpText(){
+        return helpText;
     }
     public BooleanProperty getAlert(){
         return alert;
